8b5214e963aaefa54e0628ae40bc0293ec1debb9
D.Yamasaki <lovepingpong0@gmail.com>
2023-07-31 22:01:51 +0900

    Initial Commit

---
+++ MyColorMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyColorMemoApp
+//
+//  Created by 山﨑大地 on 2023/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
